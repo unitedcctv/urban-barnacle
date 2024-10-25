30990a9af057537a9235476b5237412f406bb81f
karl <karl.burton@bigrep.com>
2024-10-25 17:45:56 +0200

    change logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import reactLogo from "./assets/react.svg";
+import urbanBarnacle from "./assets/urban_barnacle.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={urban_barnacle} className="logo react" alt="Urban Barnacle" />
+        <img src={urbanBarnacle} className="logo react" alt="Urban Barnacle" />
         <h1>Urban Barnacle</h1>
       </header>
     </div>
